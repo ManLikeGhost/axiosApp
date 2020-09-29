df3083bd96216787a3b051ed9b09b6e9d8b85946
Ekpo Eddy Edet <manlikeghost@Ekpos-MacBook-Pro.local>
2020-09-29 05:05:18 +0100

    axiosReact

---
+++ src/components/PersonList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+
+export class PersonList extends Component {
+    state ={
+        persons: [],
+    };
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+            console.log(res);
+            this.setState({ persons: res.data});
+        })
+    }
+    render() {
+        return (
+            <div>
+              <ul>
+                { this.state.persons.map(person => <li key={person.id}> {person.name} </li> ) }</ul>  
+            </div>
+        )
+    }
+}
+
+export default PersonList
